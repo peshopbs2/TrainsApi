f2db96a7c9a332622df24c6b5d81ab30009e8c73
peshopbs2 <peshopbs2@gmail.com>
2024-01-28 03:51:18 -0800

    add repository for working with data

---
+++ TrainsApi/Data/Repositories/Repository.cs
@@ -0,0 +1,65 @@
+﻿using ArtistMVC.Data.Data.Abstractions;
+using ArtistMVC.Data.Repositories.Abstractions;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtistMVC.Data.Repositories
+{
+    public class Repository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet
+                .AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate)
+                .ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State =  EntityState.Modified;
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
