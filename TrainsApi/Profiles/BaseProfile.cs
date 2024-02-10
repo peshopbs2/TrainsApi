cc4554fa040692b75bf45f60d2bb8c86fc68a31c
peshopbs2 <peshopbs2@gmail.com>
2024-02-10 04:29:44 -0800

    add base profile mapping

---
+++ TrainsApi/Profiles/BaseProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TrainsApi.Data.Entities;
+using TrainsApi.DTOs.Abstractions;
+using TrainsApi.DTOs.Requests;
+using TrainsApi.DTOs.Responses;
+
+namespace TrainsApi.Profiles
+{
+    public class BaseProfile : Profile
+    {
+        public BaseProfile()
+        {
+            CreateMap<BaseEntity, BaseResponseDTO>();
+            CreateMap<BaseRequestDTO, BaseEntity>();
+        }
+    }
+}
